97ab7e886d290747813e495ed17dcbc1c743e394
Tom Capers <tompcapers@gmail.com>
2021-07-29 12:06:45 -0700

    updated comment handle for styling

---
+++ controllers/api/dashboardRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const { Blog, User, Comment } = require('../../models/');
+const withAuth = require('../../utils/auth');
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const userBlogData = await Blog.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            }
+        });
+        if (!userBlogData) {
+            res.status(404).json({message: 'No blog found with this id!'});
+            return;
+        }
+        res.status(200).json(userBlogData);
+    }catch (err){
+        res.status(500).json(err);
+    }
+});
+
+
+
+module.exports = router;
